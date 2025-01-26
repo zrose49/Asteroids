12b01a4465083a30f99b7281024d12f3245d5b89
zrose49 <zxr110230@gmail.com>
2025-01-26 12:58:35 -0600

    Updated time in main

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    # Start of game loop
     while(1<2):
         screen.fill((0,0,0),None,0)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = (clock.tick(60))/1000
 
 if __name__ == "__main__":
     main()
