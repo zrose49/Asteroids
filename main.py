9fbc4fec3a4b3b58e4b920958e224fd5844d8ff4
zrose49 <zxr110230@gmail.com>
2025-01-26 12:32:34 -0600

    Adding initial Asteroid files

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    while(1<2):
+        screen.fill((0,0,0),None,0)
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+if __name__ == "__main__":
+    main()
